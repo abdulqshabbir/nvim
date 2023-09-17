717cd96a63f2f300eb6dbe5883061fe55090234b
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-09-17 12:23:19 +0800

    add <leader>git command

--- lua/josean/plugins/fugitive.lua
+++ lua/josean/plugins/fugitive.lua
@@ -2,5 +2,6 @@ return {
 	"tpope/vim-fugitive",
 	keys = {
 		{ "<leader>diff", "<cmd>Gvdiff<CR>", { desc = "Open a git diff horizontally" } },
+		{ "<leader>git", "<cmd>G<CR>", { desc = "Open up fugitive" } },
 	},
 }
