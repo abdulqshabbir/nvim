6a0884235abfdd618e67201e407c5d822d9c7ca0
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-09-17 12:43:11 +0800

    add more keymaps

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -24,6 +24,8 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit current window" })
+
 -- tab management
 keymap.set("n", "<leader>n", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>w", ":tabclose<CR>") -- close current tab
