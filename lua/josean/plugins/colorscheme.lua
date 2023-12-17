76c9cca4ac0059d34b3c72a71484debe1c402724
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-12-17 14:49:11 +0800

    style: change color scheme

--- lua/josean/plugins/colorscheme.lua
+++ lua/josean/plugins/colorscheme.lua
@@ -1,51 +1,47 @@
 return {
 	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-mocha",
+		},
+	},
+	{
 		"catppuccin/nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
 		name = "catppuccin",
-		config = function()
-			-- load the colorscheme here
-			local catppuccin = require("catppuccin")
-			catppuccin.setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				transparent_background = false, -- disables setting the background color.
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-				dim_inactive = {
-					enabled = true, -- dims the background color of inactive window
-					shade = "dark",
-					percentage = 0.15, -- percentage of the shade to apply to the inactive window
+		priority = 1000,
+		opts = {
+			term_colors = true,
+			transparent_background = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
 				},
-				no_italic = false, -- Force no italic
-				no_bold = false, -- Force no bold
-				no_underline = false, -- Force no underline
-				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-					comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
-					loops = {},
-					functions = {},
-					keywords = {},
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
+			},
+			integrations = {
+				telescope = {
+					enabled = true,
+					style = "nvchad",
 				},
-				color_overrides = {},
-				custom_highlights = {},
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					treesitter = true,
-					notify = false,
-					mini = false,
-					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				dropbar = {
+					enabled = true,
+					color_mode = true,
 				},
-			})
-			vim.cmd([[colorscheme catppuccin-mocha]])
-		end,
+			},
+		},
 	},
 }
