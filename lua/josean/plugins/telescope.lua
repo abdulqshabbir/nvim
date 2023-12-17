3cbf38dabb279cce839ac222dc30faa5a1675498
Abdul Shabbir <abdulqshabbir@gmail.com>
2023-12-17 14:48:38 +0800

    fix: change quick fix list mapping

--- lua/josean/plugins/telescope.lua
+++ lua/josean/plugins/telescope.lua
@@ -26,7 +26,7 @@ return {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-qf>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
 				},
 			},
